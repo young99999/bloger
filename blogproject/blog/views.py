a99eb36fc71ae88129b4a1adc9e8c8b4afb9e67e
young99999 <YOUNG@s-164-67-230-106.resnet.ucla.edu>
2017-06-01 01:23:27 -0700

    Update

---
+++ blogproject/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+from .models import Post
+
+# Create your views here.
+def index(request):
+	post_list = Post.objects.all().order_by('-created_time')
+	return render(request, 'blog/index.html', context={'post_list': post_list})

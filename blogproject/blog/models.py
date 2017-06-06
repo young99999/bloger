0dc7c9f5adadd777e485fe35d8a0ebab4f06d8aa
Yaqi1023 <yulisazhang@yulisas-MacBook-Pro.local>
2017-06-06 15:29:59 -0700

    Detail Page

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
 
 # Create your models here.
@@ -31,3 +31,8 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	# get_absolute_url method
+	# import reverse function from django.urls
+	def get_absolute_url(self):
+		return reverse('blog:detail', kwargs={'pk': self.pk})

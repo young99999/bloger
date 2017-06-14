70dbc0abbd679543d4beaee6a0d7bda362c598e5
young99999 <YOUNG@s-164-67-230-177.resnet.ucla.edu>
2017-06-14 01:28:00 -0700

    Updating detail pages

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^post/(?P<pk>[0-9] + )/$', views.detail, name='detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
 from .models import Post
+import markdown
 
 # Create your views here.
 def index(request):
@@ -10,5 +11,6 @@ def index(request):
 	return render(request, 'blog/index.html', context={'post_list': post_list})
 
 def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/detail.html', context={'post': post})
+	post = get_object_or_404(Post, pk=pk)
+	post.body = markdown.markdown(post.body, extensions=['markdown.extensions.extra','markdown.extensions.codehilite', 'markdown.extensions.toc',])
+	return render(request, 'blog/detail.html', context={'post': post})

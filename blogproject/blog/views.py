0c8d940d086d9622792825c22af16142897afc42
young99999 <YOUNG@s-164-67-81-81.resnet.ucla.edu>
2017-06-14 22:16:42 -0700

    Updating details

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
-
+from comments.forms import CommentForm
 from .models import Post, Category
 import markdown
 
@@ -12,8 +12,19 @@ def index(request):
 
 def detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
-	post.body = markdown.markdown(post.body, extensions=['markdown.extensions.extra','markdown.extensions.codehilite', 'markdown.extensions.toc',])
-	return render(request, 'blog/detail.html', context={'post': post})
+	post.body = markdown.markdown(post.body,
+								  extensions=[
+									  'markdown.extensions.extra',
+									  'markdown.extensions.codehilite',
+									  'markdown.extensions.toc',
+								  ])
+	form = CommentForm()
+	comment_list = post.comment_set.all()
+	context = {'post': post,
+			   'form': form,
+			   'comment_list': comment_list
+			   }
+	return render(request, 'blog/detail.html', context=context)
 
 def archives(request, year, month):
 	post_list = Post.objects.filter(created_time__year=year,

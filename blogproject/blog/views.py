0adf9ba3656cab7cadad4487a40a3118de529913
young99999 <YOUNG@s-164-67-81-81.resnet.ucla.edu>
2017-06-14 23:16:41 -0700

    Updating

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -37,6 +37,6 @@ def category(request, pk):
 	post_list = Post.objects.filter(category=cate).order_by('-created_time')
 	return render(request, 'blog/index.html', context={'post_list': post_list})
 
-def contact(request):
+#def contact(request):
 	#post_list = Post.objects.all().order_by('-created_time')
-	return render(request, 'blog/contact.html', context=context)
+	#return render(request, 'blog/contact.html', context=context)
